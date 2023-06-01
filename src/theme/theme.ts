84f28d02ce31c157ac07ca2f965a53a7a2443853
오하늘 <nebulass@naver.com>
2023-06-02 00:17:38 +0900

    리액트 라우터 설치, 스타일 컴포넌트 테마 설정

---
+++ src/theme/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+	black: '#000',
+	white: '#fff',
+	info: '#000',
+	primary: 'rgb(28, 36, 69)',
+	secondary: 'rgb(243, 48, 87)',
+	yellow: 'rgb(255, 219, 109)',
+};
